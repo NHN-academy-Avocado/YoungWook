014c6d18753254df1d75c24418cc97b66fbcb8eb
Pandemic23 <popofh12345@naver.com>
2023-09-14 19:56:18 +0900

    Exersize

--- 4단원/Ex6.java
+++ 4단원/Ex6.java
@@ -10,15 +10,15 @@ public class Ex6 {
         boolean b1, b2;
         //다음 각 표현에 대해 그 유형이 무엇인지 말해보세요.
         //
-        //1. b+10L
-        //2. (b+i)*l
-        //3. (b+i)*l+f
-        // 4. s/f + Math.sin(f)
-        //5. c == '비'
-        //6. l+1.5f
-        //7. 나는<10
-        //8. b1 == (f >= 5.0)
-        //9. b1 && !b2
+        //1. b+10L /A:표현식 b는 byte, 10L은 long입니다. byte와 long을 더하면 결과는 long이 됩니다.
+        //2. (b+i)*l /A:byte와 int를 곱하면 결과가 int로 승격된 다음, 여기에 long을 곱합니다.
+        //3. (b+i)*l+f /A:long에서 float인 f가 더해지는 과정에서 결과는 float이 됩니다.
+        // 4. s/f + Math.sin(f) /A:short s /float f=>float + float=> float
+        //5. c == 'b' // A:char c byte b를 비교하는 연산자이므로 boolean
+        //6. l+1.5f  //A:long+float=> float
+        //7. i<10 //A:int <10 비교 연산자 => boolean
+        //8. b1 == (f >= 5.0) //A:비교 연산자만 두개 boolean을 반환한다.
+        //9. b1 && !b2 //A: boolean and not boolean =>boolena
 
 
     }
