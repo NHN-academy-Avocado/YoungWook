657cfc9089f998005ea2e92e1abdf65fdef49161
Pandemic23 <popofh12345@naver.com>
2023-09-25 08:49:55 +0900

    Exersize 8 //Ex 7~8 Exersize 9

--- 8단원/Course8/Exersize/Ex8/Apartment.java
+++ 8단원/Course8/Exersize/Ex8/Apartment.java
@@ -1,6 +1,6 @@
-package Course8.Exersize;
+package Course8.Exersize.Ex8;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Apartment {
     public int getSize() {
@@ -8,42 +8,42 @@ public class Apartment {
     }
 
     private int size;
-    private ArrayList<String> residents; //Array list 생성 없이 만들어보기
+
+    public String getAddress() {
+        return address;
+    }
+
     private String address;
+
+    private String[] resident;
 
     public Apartment(int size, String address) {
         this.size = size;
         this.address = address;
-        this.residents = new ArrayList<>();
+        this.resident = new String[size];
     }
 
     public Apartment(String address) {
         this.address = address;
     }
 
-    public int getResidentsNum() {
-        return residents.size();
-    }
-
 
     public void addResident(String name) {
-        if (residents.size() < 10) {
-            residents.add(name);
+        for (String s : resident) {
+            if (s == null && s != name) {
+                int a = Arrays.asList(resident).indexOf(s);
+                resident[a] = name;
+                break;
+            }
         }
     }
 
     public String getResidentName(int number) {
-        if (number >= 0 && number < residents.size()) {
-            return residents.get(number);
-        } else {
-            return null;
-        }
+
     }
 
     public void removeResident(int number) {
-        if (number >= 0 && number < residents.size()) {
-            residents.remove(number);
-        }
+
 
     }
 
@@ -54,9 +54,13 @@ public class Apartment {
         sb.append("Size: ").append(size).append(" \n");
         sb.append("Address: ").append(address).append("\n");
         sb.append("Residents:\n");
-        for (int i = 0; i < residents.size(); i++) {
-            sb.append(i + 1).append(": ").append(residents.get(i)).append("\n");
+        for (String s : resident) {
+            if (s != null) {
+                sb.append((int) Arrays.asList(resident).indexOf(s) + 1 + "\t" + s + "\n");
+            }
         }
+
+
         return sb.toString();
 
     }
---
+++ 8단원/Course8/Exersize/Ex8/Test.java
@@ -0,0 +1,23 @@
+package Course8.Exersize.Ex8;
+
+public class Test {
+    public static void main(String[] args) {
+
+        Apartment myApartment = new Apartment(63, "woonamdong 60");
+        myApartment.addResident("Kim");
+        myApartment.addResident("Byeong");
+        myApartment.addResident("Evil");
+
+        System.out.println(myApartment);
+
+        System.out.println("Residents Num: " + myApartment.getResidentsNum());
+//        System.out.println("Resident 1: " + myApartment.getResidentName(1));
+//
+//        myApartment.removeResident(2);
+//        System.out.println("Remove done." + "\n");
+//        System.out.println(myApartment);
+    }
+}
+
+
+
